f30568d34cd97f2c72ab29f56143f6d5e14a241b
willson <93517421+willson1661@users.noreply.github.com>
2023-12-17 17:07:39 +0530

    Update Signup.jsx

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -5,12 +5,10 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 import MyButton from "./MyButton";
 
 const Signup = () => {
-
-
-  const [passwordShown,setPasswordShown]=useState(false);
-  const togglePasswordVisiblity=()=>{
-    setPasswordShown(passwordShown? false : true);
-  }
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
   return (
     <div class="cards">
       <div className="container">
@@ -22,31 +20,39 @@ const Signup = () => {
             <div className="underlinee"></div>
           </div>
           <div className="name">
-              <input type="text" placeholder="Name" />
-            </div>
+            <input type="text" placeholder="Name" />
+            <span className="t1"></span>
+          </div>
 
           <div className="name">
-            <input type="text" placeholder=" Email" />
+            <input
+              id="email"
+              placeholder="Email"
+              name="email"
+              type="email"
+              required
+            />
+            <span className="t1"></span>
           </div>
 
           <div className="password">
-            <input  placeholder="Password" type={passwordShown ? "text" : "password"}/>
-            <div className="eye" onClick={togglePasswordVisiblity}>{passwordShown?(<FaEye />):(<FaEyeSlash />)} </div>
+            <input
+              placeholder="Password"
+              type={passwordShown ? "text" : "password"}
+            />
+            <span className="t1"></span>
+            <div className="eye" onClick={togglePasswordVisiblity}>
+              {passwordShown ? <FaEye /> : <FaEyeSlash />}{" "}
+            </div>
+            <span className="t1"></span>
           </div>
         </div>
 
-        <button
-          className="login1"
-         
-        >
-              <MyButton to="Signup" />
-         
+        <button className="login1">
+          <MyButton to="Signup" />
         </button>
-        <button
-          className="login111"
-         
-        >
-       <MyButton  to="Login" />
+        <button className="login111">
+          <MyButton to="Login" />
         </button>
         <button className="submit">Submit</button>
       </div>
